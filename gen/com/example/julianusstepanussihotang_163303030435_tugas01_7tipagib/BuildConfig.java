f2b8a92db35308c82ffec7badcddf6ac7ce6303b
julianusstepanussihotang <belajar1.github@gmail.com>
2019-11-27 19:00:26 +0700

    TUGAS02

---
+++ gen/com/example/julianusstepanussihotang_163303030435_tugas01_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.julianusstepanussihotang_163303030435_tugas01_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
